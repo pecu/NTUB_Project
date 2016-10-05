5a2a2c039429b34ab8e2c7c112b8d9e687a077f2
pecu <pecu610@gmail.com>
2016-10-05 12:13:58 +0800

    R爬蟲

--- 2016-10-05 R/HiPtt.r
+++ 2016-10-05 R/HiPtt.r
@@ -4,7 +4,7 @@ library(RCurl)
 library(httr)
 
 startNo = 1
-endNo   = 20
+endNo   = 3
 subPath = "https://www.ptt.cc/bbs/joke/index"
 alltitle = data.frame()
 for( i in c(startNo:endNo) )
@@ -13,7 +13,19 @@ for( i in c(startNo:endNo) )
   urlPath  = paste(subPath, i, ".html", sep = "")
   temp     = getURL(urlPath)
   xmlData  = htmlParse(temp) 
-  title    = xpathSApply(xmlData, "//div[@class=\"title\"]", xmlValue)
-  subtitle = data.frame(title)
+  titleRUL = xpathSApply(xmlData, "//div[@class=\"title\"]/a//@href")
+  subtitle = data.frame(titleRUL)
   alltitle = rbind(alltitle, subtitle)
+}
+
+allPath = length(alltitle$titleRUL)
+tempURL = "https://www.ptt.cc"
+for( j in 1:allPath )
+{
+  urlPath  = paste(tempURL, alltitle$titleRUL[j], sep = "")
+  temp     = getURL(urlPath)
+  xmlData  = htmlParse(temp) 
+  Pcontent = xpathSApply(xmlData, "//div[@id=\"main-content\"]", xmlValue)
+  filename = paste("./data/", j, ".txt", sep = "")
+  write.csv(Pcontent, filename)
 }
